d5ff09efef59ba9ee74934f5dfc35fe0b2ffe9ba
randyhr318 <74457618+randyhr318@users.noreply.github.com>
2021-02-10 09:21:18 -0600

    Initial commit

---
+++ src/main/java/com/techtalentsouth/bigchiefblog/BigchiefblogApplication.java
@@ -0,0 +1,13 @@
+package com.techtalentsouth.bigchiefblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigchiefblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigchiefblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techtalentsouth/bigchiefblog/BigchiefblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtalentsouth.bigchiefblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigchiefblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
